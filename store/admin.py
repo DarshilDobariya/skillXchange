7edba8ab9905b5b33386946b23efe9366663b21f
Dhaval2908 <dhavalkhunt17@gmail.com>
2024-11-27 14:18:36 -0500

    fix the bugs for login and home page

--- store/admin.py
+++ store/admin.py
@@ -1,6 +1,16 @@
 from django.contrib import admin
 
 from .models.customer import Customer
+<<<<<<< Updated upstream
+=======
+# from .models.orders import Order
+from .models.connection import ConnectionRequest
+from .models.notifications import Notification
+from .models.chat import Message
+from .models.meetings import Meeting
+from .models.roommembers import RoomMember
+from .models.customer_ratings import CustomerRating
+>>>>>>> Stashed changes
 
 
 
